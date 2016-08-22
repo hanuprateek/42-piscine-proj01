bfde332de4d82f175ee531fe9e9eb9c8f5f1e203
Hanu Prateek Kunduru <hanu.prateek@gmail.com>
2016-08-21 20:32:42 -0700

    update code

--- ex01/nmatch.c
+++ ex01/nmatch.c
@@ -1,28 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   match.c                                            :+:      :+:    :+:   */
+/*   nmatch.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hkunduru <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2016/08/21 02:54:47 by hkunduru          #+#    #+#             */
-/*   Updated: 2016/08/21 20:22:02 by hkunduru         ###   ########.fr       */
+/*   Created: 2016/08/21 20:22:53 by hkunduru          #+#    #+#             */
+/*   Updated: 2016/08/21 20:22:59 by hkunduru         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	match(char *s1, char *s2)
+int	nmatch(char *s1, char *s2)
 {
 	if (*s1 == '\0' && *s2 == '\0')
 		return (1);
 	if (*s2 == '*')
 	{
 		if (*s1 == '\0')
-			return (match(s1, s2 + 1));
+			return (nmatch(s1, s2 + 1));
 		else
-			return (match(s1, s2 + 1) || match(s1 + 1, s2));
+			return (nmatch(s1, s2 + 1) + nmatch(s1 + 1, s2));
 	}
 	if (*s1 == '\0' || *s2 == '\0' || *s1 != *s2)
 		return (0);
 	else
-		return (match(s1 + 1, s2 + 1));
+		return (nmatch(s1 + 1, s2 + 1));
 }
